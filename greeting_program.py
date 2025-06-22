98e0241940a8636f69aa74affd25149e3751637f
Mohit <ghsmohit2004@gmail.com>
2025-06-22 19:31:40 +0530

    Update greeting_program.py
    change to chaining condition

--- greeting_program.py
+++ greeting_program.py
@@ -7,13 +7,13 @@ _name_ = input("What is your name: ").capitalize()
 time = int(time.strftime("%H%M"))
 
 # check greeting conditions.
-if (time >= 5 and time <= 1200 ):
+if  5 <= time <=1200:
     print(f"Hello {_name_} Good morning")
 
-elif (time > 1200 and time <=1600):
+elif 1200 < time <=1600:
     print(f"Hello {_name_} Good afternoon")
 
-elif (time > 1600 and time <=1959):
+elif 1600 < time <=1959:
     print(f"Hello {_name_} Good evening")
 
 else: 
