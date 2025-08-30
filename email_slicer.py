4e034f35466f05648a19faacdc602705bea99eb1
Mohit <ghsmohit2004@gmail.com>
2025-08-30 21:05:55 +0530

    Update email_slicer.py

--- email_slicer.py
+++ email_slicer.py
@@ -1,8 +1,10 @@
 #email slicer
 
+#taking input
 email = input("Enter your email : ")
 
 index = email.index('@')
 username = email[:index]
 domain = email[index+1:]
+
 print(f"The username of email {email} is {username} and domain is {domain}")
